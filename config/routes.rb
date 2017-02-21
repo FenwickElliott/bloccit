a9a6de79876656b052f2922ebf6fe1a293ddd7fd
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-21 12:23:10 -0500

    a12: contact stub added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   root 'welcome#index'
   # above is 'implied hash syntax' it equates to: root({to: 'welcome#index'})
 end
