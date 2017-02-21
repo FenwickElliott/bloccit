5b34841681370dc33562bda7a0760c87d8b9980f
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-21 12:55:22 -0500

    a13: FAQ stub added and tested

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'welcome/contact'
 
+  get 'welcome/FAQ'
+
   root 'welcome#index'
   # above is 'implied hash syntax' it equates to: root({to: 'welcome#index'})
 end
